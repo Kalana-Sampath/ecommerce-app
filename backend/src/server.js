6a616659331154fd39fba4b5f34e3d85ee71733f
Kalana <kalanas414@gmail.com>
2025-12-15 23:16:58 +0530

    startServer method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,9 +25,12 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
 
-app.listen(ENV.PORT, () => {
-  console.log("Server is up and running")
-  connectDB();
-});
+startServer();
 
