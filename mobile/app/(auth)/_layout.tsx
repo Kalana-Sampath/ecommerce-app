67c9eb4074f573c6cf5a6ce44ed504375a1098cd
Kalana <kalanas414@gmail.com>
2025-12-16 20:42:17 +0530

    set up tab navigation with styles

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -2,12 +2,13 @@ import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
+  if (!isLoaded) return null;  // for a better ux
   
   if (isSignedIn) {
-    return <Redirect href={"/"} />;
+    return <Redirect href={"/(tabs)"} />;
   }
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}6
+}
---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SafeScreen from '@/components/SafeScreen'
+
+const ShopScreen = () => {
+  return (
+    <SafeScreen>
+      <View>
+        <Text className='text-white'>ShopScreen</Text>
+      </View>
+    </SafeScreen>
+  );
+};
+
+export default ShopScreen
