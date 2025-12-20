3bc79ee17f4d97b8e47c4c80ac1bb8540280f62a
Kalana <kalanas414@gmail.com>
2025-12-20 16:48:34 +0530

    profile and cart screen setup with route pages

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ import orderRoutes from "./routes/order.route.js";
 import reviewRoutes from "./routes/review.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
-
+import paymentRoutes from "./routes/payment.route.js"
 
 const app = express();
 
@@ -35,6 +35,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/payment", paymentRoutes)
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
